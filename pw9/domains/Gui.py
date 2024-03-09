cdc9df7a393e4b8241c39d78d951f75502acabca
crylimystics <QuanND.22BI13371@usth.edu.vn>
2024-03-09 19:46:06 +0700

    Added Grade tab

--- pw9/domains/Gui.py
+++ pw9/domains/Gui.py
@@ -2,10 +2,8 @@ import threading
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
-
-from Utilities import *
-
 from . import Student, School, Course
+from Utilities import pickletool, compression
 
 
 class Gui:
@@ -20,10 +18,12 @@ class Gui:
         self.tab_student = ttk.Frame(self.notebook)
         self.tab_course = ttk.Frame(self.notebook)
         self.tab_grade = ttk.Frame(self.notebook)
+        self.tab_grade_entry = ttk.Frame(self.notebook)
 
         self.notebook.add(self.tab_student, text="Students")
         self.notebook.add(self.tab_course, text="Courses")
         self.notebook.add(self.tab_grade, text="Grades")
+        self.notebook.add(self.tab_grade_entry, text="Grades entry")
 
         self.notebook.pack(expand=1, fill="both")
 
@@ -68,6 +68,10 @@ class Gui:
         # self.grade_label.grid(row=2, column=0)
         # self.grade_entry = tk.Entry(root)
         # self.grade_entry.grid(row=2, column=1)
+        self.course_var = tk.StringVar()
+        self.course_dropdown = ttk.Combobox(self.tab_grade, textvariable=self.course_var)
+        self.course_dropdown.grid(row=3, column=0, columnspan=4)
+        self.course_dropdown.bind('<FocusIn>', lambda event: self.populate_course_dropdown())
 
         # Button to add information to list box
         self.add_button_student = tk.Button(self.tab_student, text="Add", command=lambda: self.add_student_info(school))
@@ -75,6 +79,9 @@ class Gui:
 
         self.add_button_course = tk.Button(self.tab_course, text="Add", command=lambda: self.add_course_info(school))
         self.add_button_course.grid(row=3, column=0, columnspan=2)
+
+        self.add_button_grade = ttk.Button(self.tab_grade, text="Submit Grades", command= self.create_grade_input)
+        self.add_button_grade.grid(row=4, column=0, columnspan=2)
 
         # List box to display information
         self.listbox_student = tk.Listbox(self.tab_student, width=100)
@@ -84,7 +91,7 @@ class Gui:
         self.listbox_course.grid(row=4, column=0, columnspan=2)
 
         self.listbox_grade = tk.Listbox(self.tab_grade, width=100)
-        self.listbox_grade.grid(row=4, column=0, columnspan=2)
+        self.listbox_grade.grid(row=6, column=0, columnspan=2)
 
         # Check if there are any already existing data and add to list box
         if len(school.getstudents()) > 0:
@@ -133,6 +140,44 @@ class Gui:
         self.listbox_course.insert(tk.END, f"Course ID: {course_id}, Course Name: {course_name}")
         self.course_name_entry.delete(0, tk.END)
         self.course_id_entry.delete(0, tk.END)
+
+    # populate dropdown menu with courses
+    def populate_course_dropdown(self):
+        courses = self.school.getcourses()
+        course_ids= [course.getid() for course in courses]
+        self.course_dropdown['values'] = course_ids
+
+
+    def create_grade_input(self):
+        for widget in self.tab_grade_entry.winfo_children():
+            widget.destroy()
+
+        self.grade_entries = []
+
+        for student in self.school.getstudents():
+            label = ttk.Label(self.tab_grade_entry, text=student.getname())
+            # label.grid(row=2, column=3)
+            label.pack()
+            entry = ttk.Entry(self.tab_grade_entry)
+            # entry.grid(row=3, column=3)
+            entry.pack()
+            self.grade_entries.append(entry)
+
+        self.add_button_grade_entry = ttk.Button(self.tab_grade_entry, text="Add", command=self.submit_grades)
+        # self.add_button_grade_entry.grid(row=4, column=0, columnspan=2)
+        self.add_button_grade_entry.pack()
+
+
+
+    def submit_grades(self):
+        selected_course = self.course_var.get()
+        grades = [entry.get() for entry in self.grade_entries]
+        for student, grade in zip(self.school.getstudents(), grades):
+            student.add_mark(selected_course, grade)
+
+            # add entered grades to grade tab
+            self.listbox_grade.insert(tk.END, f"Student {student.getid()}, course {selected_course}:"
+                                                       f" {grade}")
 
     def savefile(self):
         save_thread = threading.Thread(target=pickletool.storeData, args=(self.school,))
